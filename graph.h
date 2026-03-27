e7a3926ddeb4d7bd539308df142acc70111ed1e2
nellakarczewicz <01199367@pw.edu.pl>
2026-03-27 16:35:42 +0100

    fix(graph): add I/O and memory management prototypes to header

--- graph.h
+++ graph.h
@@ -1,39 +1,30 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-// Definicja struktur wierzchołka, krawędzi oraz grafu
-
-/**
- * Struktura reprezentująca wierzchołek grafu (Node)
- */
 typedef struct {
-    int id;             // oryginalne ID z pliku wejściowego
-    double x, y;        // aktualne współrzędne
-    
-    // do Fruchterman-Reingold
-    double dx, dy;      // wektory sił (przemieszczenia) w danej iteracji
-
+    int id;
+    double x, y;
+    double dx, dy; 
 } Node;
 
-/**
- * Struktura reprezentująca krawędź grafu (Edge)
- */
 typedef struct {
-    int u_idx;          // indeks wierzchołka źródłowego w tablicy nodes[]
-    int v_idx;          // indeks wierzchołka docelowego w tablicy nodes[]
-    double weight;      // waga krawędzi (z pliku wejściowego)
+    int u_idx;
+    int v_idx;
+    double weight;
 } Edge;
 
-/**
- * Główna struktura przechowująca cały graf
- */
 typedef struct {
-    Node *nodes;        // dynamiczna tablica wierzchołków
-    Edge *edges;        // dynamiczna tablica krawędzi
-    int node_count;     // liczba wierzchołków
-    int edge_count;     // liczba krawędzi
-    double width;       // szerokość obszaru roboczego
-    double height;      // wysokość obszaru roboczego
+    Node *nodes;
+    Edge *edges;
+    int node_count;
+    int edge_count;
+    double width;
+    double height;
 } Graph;
 
-#endif // GRAPH_H
+//do czytania i zapisywania itd
+int read_graph(Graph *g, const char *filename);
+int save_graph(Graph *g, const char *filename, const char *format);
+void free_graph(Graph *g);
+
+#endif
